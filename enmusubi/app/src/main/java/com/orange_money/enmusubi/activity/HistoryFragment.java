4bbd50bdff4686d17c63124b04195e8f875f29a3
asuka <asuka2917@gmail.com>
2014-11-18 19:31:42 +0900

    トグルスイッチ以外の履歴画面の実装

--- enmusubi/app/src/main/java/com/orange_money/enmusubi/activity/HistoryFragment.java
+++ enmusubi/app/src/main/java/com/orange_money/enmusubi/activity/HistoryFragment.java
@@ -7,8 +7,15 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.orange_money.enmusubi.HistoryAdapter;
 import com.orange_money.enmusubi.R;
+import com.orange_money.enmusubi.TextAdapter;
+import com.orange_money.enmusubi.TextData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -26,8 +33,39 @@ public class HistoryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_history, container, false);
+
+        //この下にテキスト詳細を取得してviewにセットする処理を書く．
+
+        //テキストデータの生成
+        List<TextData> texts = new ArrayList<TextData>();
+
+        //以下テストデータ
+        TextData item1 = new TextData();
+        item1.setTextId(1);
+        item1.setTextTitle("huga");
+
+        TextData item2 = new TextData();
+        item2.setTextId(2);
+        item2.setTextTitle("huga");
+
+
+        texts.add(item1);
+        texts.add(item2);
+        //ここまで
+
+
+        //viewにテキストデータをセット
+        ListView textView = (ListView)v.findViewById(R.id.historyListView);
+
+        HistoryAdapter historyAdapter = new HistoryAdapter(v.getContext(),texts);
+        textView.setAdapter(historyAdapter);
+
+
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false);
+        return v;
     }
 
 
